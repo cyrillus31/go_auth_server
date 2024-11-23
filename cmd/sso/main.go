a1142e4c66abfaab528845e7d54f0171410c05ae
cyrillus <kirill.olegovich31@gmail.com>
2024-11-23 16:10:12 +0300

    main(task): setup a Taskfile to start main app

--- cmd/sso/main.go
+++ cmd/sso/main.go
@@ -1,11 +1,19 @@
-package sso
+package main
+
+import (
+	"fmt"
+
+	"github.com/cyrillus31/go_auth_server/internal/config"
+)
 
 func main() {
-  // TODO: инициализировать объект конфига
+	// TODO: инициализировать объект конфига
+	cfg := config.MustLoad()
+	fmt.Println(cfg)
 
-  // TODO: инициализировать логгер
+	// TODO: инициализировать логгер
 
-  // TODO: инициализировать приложоения-сервис (app)
+	// TODO: инициализировать приложоения-сервис (app)
 
-  // TODO: запустить gRPC сервер
+	// TODO: запустить gRPC сервер
 }
